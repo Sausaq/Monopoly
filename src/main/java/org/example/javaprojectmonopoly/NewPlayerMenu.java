1f2610a6be7d764e60793e949063422f47d84ddf
Aqylbek <akilshalabayev@gmail.com>
2024-05-06 19:28:23 +0600

    Модифицировал передачу файлов

--- src/main/java/org/example/javaprojectmonopoly/NewPlayerMenu.java
+++ src/main/java/org/example/javaprojectmonopoly/NewPlayerMenu.java
@@ -30,9 +30,8 @@ public class NewPlayerMenu extends StackPane {
         VBox fields = new VBox();
         fields.setMaxSize(944, 638);
         ImageView monopoly = new ImageView("file:/D:/IntelijIDEA_Projects/JavaProjectMonopoly/src/main/resources/org/example/javaprojectmonopoly/monopoly_icon.png");
-        Font beaumaris = Font.loadFont("file:/D:/IntelijIDEA_Projects/JavaProjectMonopoly/src/main/java/org/example/javaprojectmonopoly/css/BeaumarisDemo-Regular.ttf", 30);
-        Font peaceSans = Font.loadFont("file:/D:/IntelijIDEA_Projects/JavaProjectMonopoly/src/main/java/org/example/javaprojectmonopoly/css/peaceSans.ttf", 36);
-
+        Font peaceSans = Font.loadFont(getClass().getResourceAsStream("src/main/java/org/example/javaprojectmonopoly/css/peaceSans.ttf"), 36);
+        Font beaumaris = Font.loadFont(getClass().getResourceAsStream("src/main/java/org/example/javaprojectmonopoly/css/peaceSans.ttf"), 36);
 
         monopoly.setFitWidth(140);
         monopoly.setFitHeight(140);
